42781169e6e1f28885621f69b2189f8742ea706e
Mary Brady <emceebrady@gmail.com>
2018-09-20 17:04:54 -0600

    rock paper scissors game

---
+++ Models/Game.cs
@@ -0,0 +1,9 @@
+namespace rock_paper_scissors.Models
+{
+    public class Game
+    {
+        public string Computer { get; set; }
+        public string Player { get; set; }
+        public bool isWinner { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using rock_paper_scissors.Models;
+
+
+namespace rock_paper_scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RPS TOWN!");
+            List<string> choices = new List<string>() { "rock", "paper", "scissors" };
+            bool playing = true;
+
+            Random random = new Random();
+            int ComputerChoice = random.Next(0, 3);
+            while (playing)
+            {
+                Console.WriteLine("Choose your weapon:");
+
+                choices.ForEach(choice => Console.WriteLine(choice));
+
+                string PlayerChoice = Console.ReadLine().ToLower();
+
+                if (PlayerChoice == choices[ComputerChoice])
+                {
+                    Console.WriteLine("Tie game...Play Again? (Y/N)");
+                }
+
+                if (PlayerChoice == "rock")
+                {
+                    if (ComputerChoice == 1)
+                    {
+                        Console.WriteLine($"Computer chose {ComputerChoice}, Computer wins! Play again? (Y/N)");
+                        string response = Console.ReadLine();
+                        if (response.ToUpper() != "Y")
+                        {
+                            playing = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You chose {PlayerChoice}! You Win! Computer chose {ComputerChoice}. Play again? (Y/N)");
+                        string response = Console.ReadLine();
+                        if (response.ToUpper() != "Y")
+                        {
+                            playing = false;
+                        }
+                    }
+                }
+                else if (PlayerChoice == "paper")
+                {
+                    if (ComputerChoice == 2)
+                    {
+                        Console.WriteLine($"Computer chose {ComputerChoice}, Computer wins! Play again? (Y/N)");
+                        string response = Console.ReadLine();
+                        if (response.ToUpper() != "Y")
+                        {
+                            playing = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You chose {PlayerChoice}! You Win! Computer chose {ComputerChoice}. Play again? (Y/N)");
+                        string response = Console.ReadLine();
+                        if (response.ToUpper() != "Y")
+                        {
+                            playing = false;
+                        }
+                    }
+                }
+                else
+                {
+                    if (PlayerChoice == "scissors")
+                    {
+                        Console.WriteLine($"Computer chose {ComputerChoice}, Computer wins! Play again? (Y/N)");
+                        string response = Console.ReadLine();
+                        if (response.ToUpper() != "Y")
+                        {
+                            playing = false;
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+}
